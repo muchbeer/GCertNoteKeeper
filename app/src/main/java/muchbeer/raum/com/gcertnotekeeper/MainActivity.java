0bb84bd7d144ee336744f88425838178c3c11d35
muchbeer <gman2them@gmail.com>
2019-07-07 22:31:19 +0300

    Scholarship ahead, NavigationView and SettingActivity and RecyclerView not to mention the Testing

--- app/src/main/java/muchbeer/raum/com/gcertnotekeeper/MainActivity.java
+++ app/src/main/java/muchbeer/raum/com/gcertnotekeeper/MainActivity.java
@@ -1,10 +1,12 @@
 package muchbeer.raum.com.gcertnotekeeper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceManager;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,6 +43,8 @@ spinnerCourse.setAdapter(adapterCourses);
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
--- app/src/main/java/muchbeer/raum/com/gcertnotekeeper/NavigatActivity.java
+++ app/src/main/java/muchbeer/raum/com/gcertnotekeeper/NavigatActivity.java
@@ -1,22 +1,26 @@
 package muchbeer.raum.com.gcertnotekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.preference.PreferenceManager;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -25,6 +29,7 @@ import muchbeer.raum.com.gcertnotekeeper.data.CourseRecyclerAdapter;
 import muchbeer.raum.com.gcertnotekeeper.data.DataManager;
 import muchbeer.raum.com.gcertnotekeeper.data.NoteInfo;
 import muchbeer.raum.com.gcertnotekeeper.data.NoteRecyclerAdapter;
+import muchbeer.raum.com.gcertnotekeeper.data.SettingsActivity;
 
 public class NavigatActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -64,6 +69,10 @@ public class NavigatActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         initializeDisplayContent();
+
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        // PreferenceManager.setDefaultValues(this,  R.xml.pref_notification, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_data_sync, false);
     }
 
 
@@ -72,7 +81,24 @@ public class NavigatActivity extends AppCompatActivity
         super.onResume();
 
         mNoteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
     }
+
+    private void updateNavHeader() {
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView textUserName = (TextView)headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAddress = (TextView)headerView.findViewById(R.id.text_email_address);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = pref.getString("user_display_name", "");
+        String emailAddress = pref.getString("user_email_address", "");
+
+        textUserName.setText(userName);
+        textEmailAddress.setText(emailAddress);
+    }
+
 
     private void initializeDisplayContent() {
         mRecyclerItems = (RecyclerView) findViewById(R.id.list_items);
@@ -136,6 +162,7 @@ public class NavigatActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
@@ -155,8 +182,9 @@ public class NavigatActivity extends AppCompatActivity
          displayCourses();
         }
         else if (id == R.id.nav_share) {
-
+            handleShare();
         } else if (id == R.id.nav_send) {
+            handleSelection(R.id.action_send_mail);
 
         }
 
@@ -165,8 +193,15 @@ public class NavigatActivity extends AppCompatActivity
         return true;
     }
 
-    private void handleSelection(String message) {
+    private void handleShare() {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view, "Share to - " +
+                        PreferenceManager.getDefaultSharedPreferences(this).getString("user_favorite_social", ""),
+                Snackbar.LENGTH_LONG).show();
+    }
+
+    private void handleSelection(int message_id) {
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view, message_id, Snackbar.LENGTH_LONG).show();
     }
 }
